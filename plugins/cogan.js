2be3fad3b695c0344f165bfc3dc8925be0574938
TenZul42_ <80806879+dioooo-251@users.noreply.github.com>
2024-06-19 00:25:45 +0800

    Update cogan.js

--- plugins/cogan.js
+++ plugins/cogan.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zahwazein.xyz/randomimage/cogan?apikey=zenzkey_f07b21f698', 'cogan.mp4', 'Nih kak', m)
+	conn.sendFile(m.chat, 'https://api.zahwazein.xyz/randomimage/cogan?apikey=zenzkey_f07b21f698', 'cogan.jpg', 'Nih kak', m)
 }
 handler.help = ['cogan']
 handler.tags = ['internet']
